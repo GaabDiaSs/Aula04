bb04e6c533be5f5340b2aa0313826d422f89f668
GaabDiaSs <gabriel.c.dias22@gmail.com>
2023-09-18 09:12:22 -0300

    first commit

---
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -0,0 +1,13 @@
+<div class="topo">
+    <div class="logo">
+        <img src="{{ asset('img/logo.png') }}">
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a href="{{ route('site.index') }}"></a></li>
+            <li><a href="{{ route('site.sobrenos') }}"></a></li>
+            <li><a href="{{ route('site.contato') }}"></a></li>
+        </ul>
+    </div>
+</div>
